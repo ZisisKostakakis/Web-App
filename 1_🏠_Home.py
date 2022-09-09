9512dcd90d9b8b07f084a54529732c31bd50fb8d
Zisis Kostakakis <zisis.kostakakis@stormharvester.com>
2022-09-09 15:11:15 +0300

    Session state

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -7,3 +7,12 @@ st.set_page_config(
 
 st.title("Main Page")
 st.sidebar.success("Welcome to the main page!")
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] = ""
+    
+my_input = st.text_input("Input a text here",st.session_state["my_input"])
+submit = st.button("Submit")
+if submit:
+    st.session_state["my_input"] = my_input
+    st.write("You submitted:", my_input)
